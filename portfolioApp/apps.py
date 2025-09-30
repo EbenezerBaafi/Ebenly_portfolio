a2e0227a849e18550621708d9a617f8d2832ef2d
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-09-30 03:57:29 +0000

    initial commit

--- portfolioApp/apps.py
+++ portfolioApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PortfolioConfig(AppConfig):
+class PortfolioappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Portfolio'
+    name = 'portfolioApp'
