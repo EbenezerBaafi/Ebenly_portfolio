946110f17edc7f66b9b64ce058590335bb449302
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-10-08 02:36:57 +0000

    added home and contact views

--- Base/views.py
+++ Base/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+def contact(request):
+    if request.method=="POST":
+        print('Post')
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        number = request.POST.get('number')
+        content = request.POST.get('content')
+        print(name,email,number,content)
+
+
