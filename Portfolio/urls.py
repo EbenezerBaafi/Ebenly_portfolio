01ec44f99cc923acdb35df4d1143a4238fced70b
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-10-08 02:03:27 +0000

    Added models

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -2,9 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Root URL now points to home
-    path('projects/', views.project_index, name='project_index'),
-    path('project/<int:pk>/', views.project_detail, name='project_detail'),
-    path('contact/', views.Contact, name='contact'),
-    path('contact/success/', views.contact_success, name='contact_success'),
 ]
--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'base/templates'),
+            os.path.join(BASE_DIR, 'Portfolio/Base/templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
