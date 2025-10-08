ba52348f81e8fe12dcc44322c955502e0a4571e7
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-10-08 02:13:27 +0000

    Added the base app

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'Portfolio/Base/templates'),
+            os.path.join(BASE_DIR, 'Portfolio/../Base/templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
